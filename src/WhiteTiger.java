1a709af92bb4d30fb4623e1579395d054626a5b7
Farrah Carter <FCarter@talentpath.com>
2020-05-18 07:11:59 -0400

    Adjusted white tiger class

--- src/WhiteTiger.java
+++ src/WhiteTiger.java
@@ -22,15 +22,11 @@ public class WhiteTiger extends Tiger{
     }
 
     public Critter.Action getMove(CritterInfo info) {
-        if (info.getFront() == Neighbor.OTHER) {
+
+        if (super.getMove(info) == Action.INFECT){
             this.infected = true;
-            return Action.INFECT;
-        } else if (info.getFront() == Neighbor.WALL || info.getRight() == Neighbor.WALL) {
-            return Action.LEFT;
-        } else if (info.getFront() == Neighbor.SAME) {
-            return Action.RIGHT;
-        } else {
-            return Action.HOP;
         }
+
+        return super.getMove(info);
     }
 }
